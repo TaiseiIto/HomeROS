54ec99163251ee799de1c02008ae24c2da239eb3
TaiseiIto <taisei1212@outlook.jp>
2026-07-21 00:09:41 +0900

    Refactor

--- xtask/src/lib.rs
+++ xtask/src/lib.rs
@@ -7,3 +7,44 @@ pub mod format;
 pub mod git;
 pub mod lint;
 mod time;
+
+pub enum Command {
+    Build,
+    Disassemble(disassemble::Command),
+    Environment(environment::Command),
+    Lint,
+    PreCommit,
+    Run,
+}
+
+impl Command {
+    pub fn run(self) {
+        match std::env::args().into() {
+            Self::Build => build::all(),
+            Self::Disassemble(disassemble) => disassemble.run(),
+            Self::Environment(environment) => environment.run(),
+            Self::Lint => lint::all(),
+            Self::PreCommit => {
+                lint::all();
+                format::all();
+                git::add_rust_sources();
+            }
+            Self::Run => unimplemented!(),
+        }
+    }
+}
+
+impl From<std::env::Args> for Command {
+    fn from(mut args: std::env::Args) -> Self {
+        args.next();
+        match args.next().unwrap().as_str() {
+            "build" => Self::Build,
+            "disassemble" => Self::Disassemble(args.into()),
+            "environment" => Self::Environment(args.into()),
+            "lint" => Self::Lint,
+            "precommit" => Self::PreCommit,
+            "run" => Self::Run,
+            arg => panic!("arg = {}", arg),
+        }
+    }
+}
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,47 +1,6 @@
-use xtask::{build, disassemble, environment, format, git, lint};
+use xtask::Command;
 
 fn main() {
     let command: Command = std::env::args().into();
     command.run();
-}
-
-enum Command {
-    Build,
-    Disassemble(disassemble::Command),
-    Environment(environment::Command),
-    Lint,
-    PreCommit,
-    Run,
-}
-
-impl Command {
-    fn run(self) {
-        match std::env::args().into() {
-            Self::Build => build::all(),
-            Self::Disassemble(disassemble) => disassemble.run(),
-            Self::Environment(environment) => environment.run(),
-            Self::Lint => lint::all(),
-            Self::PreCommit => {
-                lint::all();
-                format::all();
-                git::add_rust_sources();
-            }
-            Self::Run => unimplemented!(),
-        }
-    }
-}
-
-impl From<std::env::Args> for Command {
-    fn from(mut args: std::env::Args) -> Self {
-        args.next();
-        match args.next().unwrap().as_str() {
-            "build" => Self::Build,
-            "disassemble" => Self::Disassemble(args.into()),
-            "environment" => Self::Environment(args.into()),
-            "lint" => Self::Lint,
-            "precommit" => Self::PreCommit,
-            "run" => Self::Run,
-            arg => panic!("arg = {}", arg),
-        }
-    }
 }
