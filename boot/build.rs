ecfd4a7af5aaa94984737f1b2292cd7be4c1bbd1
TaiseiIto <taisei1212@outlook.jp>
2026-07-20 15:28:28 +0900

    Separate entry point between UEFI and OpenSBI #3

---
+++ boot/build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("cargo:rustc-check-cfg=cfg(firmware, values(\"open_sbi\",\"uefi\"))");
+    println!(
+        "cargo:rustc-cfg=firmware=\"{}\"",
+        match std::env::var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
+            "aarch64" | "x86_64" => "uefi",
+            "riscv64" => "open_sbi",
+            _ => unimplemented!(),
+        }
+    );
+}
--- boot/src/main.rs
+++ boot/src/main.rs
@@ -1,8 +1,19 @@
 #![no_main]
 #![no_std]
 
+#[cfg(firmware = "open_sbi")]
+#[no_mangle]
+pub extern "C" fn open_sbi_main() {
+    main();
+}
+
+#[cfg(firmware = "uefi")]
 #[unsafe(no_mangle)]
 fn efi_main() {
+    main();
+}
+
+fn main() {
     unimplemented!();
 }
 
