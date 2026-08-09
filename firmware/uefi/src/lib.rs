4b976fa959ddf753df505b6fb63eec86c9de00e3
TaiseiIto <taisei1212@outlook.jp>
2026-08-09 19:54:40 +0900

    Refactor

--- firmware/uefi/src/lib.rs
+++ firmware/uefi/src/lib.rs
@@ -4,7 +4,6 @@ mod runtime;
 mod simple;
 pub mod system;
 mod table;
-mod text;
 
 /// # References
 /// * [CHAR16](https://uefi.org/specs/UEFI/2.11/02_Overview.html#data-types)
--- firmware/uefi/src/simple/text/output.rs
+++ firmware/uefi/src/simple/text/output.rs
@@ -1,4 +1,4 @@
-use crate::text;
+use crate::{Char16, Status};
 
 /// # References
 /// * [SIMPLE_TEXT_OUTPUT_MODE](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol)
@@ -16,14 +16,49 @@ pub struct Mode {
 /// * [EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol)
 #[repr(C)]
 pub struct Protocol {
-    reset: text::Reset,
-    output_string: text::String,
-    test_string: text::TestString,
-    query_mode: text::QueryMode,
-    set_mode: text::set::Mode,
-    set_attribute: text::set::Attribute,
-    clear_screen: text::ClearScreen,
-    set_cursor_position: text::set::CursorPosition,
-    enable_cursor: text::EnableCursor,
+    reset: Reset,
+    output_string: String,
+    test_string: TestString,
+    query_mode: QueryMode,
+    set_mode: SetMode,
+    set_attribute: SetAttribute,
+    clear_screen: ClearScreen,
+    set_cursor_position: SetCursorPosition,
+    enable_cursor: EnableCursor,
     mode: *const Mode,
 }
+
+/// # References
+/// * [EFI_TEXT_CLEAR_SCREEN](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol-clearscreen)
+pub type ClearScreen = extern "efiapi" fn(*const Protocol) -> Status;
+
+/// # References
+/// * [EFI_TEXT_ENABLE_CURSOR](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol-enablecursor)
+pub type EnableCursor = extern "efiapi" fn(*const Protocol, bool) -> Status;
+
+/// # References
+/// * [EFI_TEXT_QUERY_MODE](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol-querymode)
+pub type QueryMode = extern "efiapi" fn(*const Protocol, usize, *mut usize, *mut usize) -> Status;
+
+/// # References
+/// * [EFI_TEXT_RESET](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol-reset)
+pub type Reset = extern "efiapi" fn(*const Protocol, bool) -> Status;
+
+/// # References
+/// * [EFI_TEXT_SET_ATTRIBUTE](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol-setattribute)
+pub type SetAttribute = extern "efiapi" fn(*const Protocol, usize) -> Status;
+
+/// # References
+/// * [EFI_TEXT_SET_CURSOR_POSITION](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol-setcursorposition)
+pub type SetCursorPosition = extern "efiapi" fn(*const Protocol, usize, usize) -> Status;
+
+/// # References
+/// * [EFI_TEXT_SET_MODE](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol-setmode)
+pub type SetMode = extern "efiapi" fn(*const Protocol, usize) -> Status;
+/// # References
+/// * [EFI_TEXT_STRING](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol-outputstring)
+pub type String = extern "efiapi" fn(*const Protocol, *const Char16) -> Status;
+
+/// # References
+/// * [EFI_TEST_TEST_STRING](https://uefi.org/specs/UEFI/2.11/12_Protocols_Console_Support.html#efi-simple-text-output-protocol-teststring)
+pub type TestString = extern "efiapi" fn(*const Protocol, *const Char16) -> Status;
