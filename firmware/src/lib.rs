21e78d6f68ce93ca844d23b67281727d4a7c21e1
TaiseiIto <taisei1212@outlook.jp>
2026-08-10 21:23:39 +0900

    Add firmware global #5

--- firmware/src/lib.rs
+++ firmware/src/lib.rs
@@ -2,3 +2,24 @@
 
 #[cfg(firmware = "uefi")]
 pub use uefi;
+
+pub struct Global {
+    #[cfg(firmware = "uefi")]
+    image_handle: uefi::Handle,
+    #[cfg(firmware = "uefi")]
+    system_table: &'static uefi::system::Table,
+}
+
+impl Global {
+    pub unsafe fn new(
+        #[cfg(firmware = "uefi")] image_handle: uefi::Handle,
+        #[cfg(firmware = "uefi")] system_table: *const uefi::system::Table,
+    ) -> Self {
+        Self {
+            #[cfg(firmware = "uefi")]
+            image_handle,
+            #[cfg(firmware = "uefi")]
+            system_table: unsafe { &*system_table },
+        }
+    }
+}
