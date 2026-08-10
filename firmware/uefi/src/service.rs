3b02c86dc37d8ee3f17151f089f9aa34686fd438
TaiseiIto <taisei1212@outlook.jp>
2026-08-10 11:41:03 +0900

    Add UEFI GetNextHighMonotonicCount #5

--- firmware/uefi/src/service.rs
+++ firmware/uefi/src/service.rs
@@ -1,21 +1,8 @@
 use crate::table;
 
 mod memory;
+mod runtime;
 mod time;
 mod variable;
 
-/// # References
-/// * [EFI_RUNTIME_SERVICES](https://uefi.org/specs/UEFI/2.11/04_EFI_System_Table.html#efi-runtime-services)
-#[repr(C)]
-pub struct Runtime {
-    hdr: table::Header,
-    get_time: time::Get,
-    set_time: time::Set,
-    get_wakeup_time: time::GetWakeup,
-    set_wakeup_time: time::SetWakeup,
-    set_virtual_address_map: memory::address::Set,
-    convert_pointer: memory::address::Get,
-    get_variable: variable::Get,
-    get_next_variable_name: variable::Next,
-    set_variable: variable::Set,
-}
+pub use runtime::Runtime;
---
+++ firmware/uefi/src/service/runtime.rs
@@ -0,0 +1,25 @@
+use {
+    super::{memory, time, variable},
+    crate::{Status, table},
+};
+
+/// # References
+/// * [GetNextHighMonotonicCount](https://uefi.org/specs/UEFI/2.11/08_Services_Runtime_Services.html#getnexthighmonotoniccount)
+pub type GetNextHighMonotonicCount = extern "efiapi" fn(*mut u32) -> Status;
+
+/// # References
+/// * [EFI_RUNTIME_SERVICES](https://uefi.org/specs/UEFI/2.11/04_EFI_System_Table.html#efi-runtime-services)
+#[repr(C)]
+pub struct Runtime {
+    hdr: table::Header,
+    get_time: time::Get,
+    set_time: time::Set,
+    get_wakeup_time: time::GetWakeup,
+    set_wakeup_time: time::SetWakeup,
+    set_virtual_address_map: memory::address::Set,
+    convert_pointer: memory::address::Get,
+    get_variable: variable::Get,
+    get_next_variable_name: variable::Next,
+    set_variable: variable::Set,
+    get_next_high_monotonic_count: GetNextHighMonotonicCount,
+}
