d780ae15839815d15970978df7251397429b57e8
TaiseiIto <taisei1212@outlook.jp>
2026-08-11 15:28:38 +0900

    Refactor

--- arch/src/lib.rs
+++ arch/src/lib.rs
@@ -1,14 +1,23 @@
 #![no_std]
 
-#[cfg(target_arch = "aarch64")]
-mod aarch64;
-#[cfg(target_arch = "aarch64")]
-pub use aarch64::*;
-#[cfg(target_arch = "riscv64")]
-mod riscv64;
-#[cfg(target_arch = "riscv64")]
-pub use riscv64::*;
-#[cfg(target_arch = "x86_64")]
-mod x64;
-#[cfg(target_arch = "x86_64")]
-pub use x64::*;
+use core::arch::asm;
+
+/// # Safety
+/// This function stops CPU until the next interruption.
+/// # References
+/// * [WFI](https://support.arm.com/documentation/ddi0487/mc/-Part-C-The-AArch64-Instruction-Set/-Chapter-C6-A64-Base-Instruction-Descriptions/-C6-2-Alphabetical-list-of-A64-base-instructions/-C6-2-504-WFI?lang=en)
+/// * [RISC-V Privileged Instruction Set Listings](https://docs.riscv.org/reference/isa/v20260120/priv/priv-insns.html)
+/// * [Intel 64 and IA-32 Architectures Software Developer's Manual June 2026](https://cdrdv2.intel.com/v1/dl/getContent/671200) p.1143
+#[inline(always)]
+pub unsafe fn wait_for_interrupt() {
+    unsafe {
+        asm!(
+            #[cfg(target_arch = "aarch64")]
+            "wfi",
+            #[cfg(target_arch = "riscv64")]
+            "wfi",
+            #[cfg(target_arch = "x86_64")]
+            "hlt",
+        );
+    }
+}
