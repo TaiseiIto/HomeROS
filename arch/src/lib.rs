b08490c95240d4a536ee0170319d94921e2fa22e
TaiseiIto <taisei1212@outlook.jp>
2026-08-11 17:23:33 +0900

    Add COM1 #5

--- arch/src/lib.rs
+++ arch/src/lib.rs
@@ -11,13 +11,11 @@ use core::arch::asm;
 #[inline(always)]
 pub unsafe fn wait_for_interrupt() {
     unsafe {
-        asm!(
-            #[cfg(target_arch = "aarch64")]
-            "wfi",
-            #[cfg(target_arch = "riscv64")]
-            "wfi",
-            #[cfg(target_arch = "x86_64")]
-            "hlt",
-        );
+        #[cfg(target_arch = "aarch64")]
+        asm!("wfi");
+        #[cfg(target_arch = "riscv64")]
+        asm!("wfi");
+        #[cfg(target_arch = "x86_64")]
+        asm!("hlt");
     }
 }
--- device/rs232c/src/lib.rs
+++ device/rs232c/src/lib.rs
@@ -8,9 +8,16 @@ pub fn write_string(string: &str) {
     }
 }
 
+/// # TODO
+/// This is an ad hoc implementation working on only QEMU.
+/// Implement RS232C completely.
 fn write_byte(byte: u8) {
-    #[cfg(target_arch = "x86_64")]
     unsafe {
-        asm!("out dx, al", in("dx") 0x02f8, in("al") byte);
+        #[cfg(target_arch = "aarch64")]
+        asm!("strb w9, x10", in("w9") byte, in("x10") 0x09000000);
+        #[cfg(target_arch = "riscv64")]
+        asm!("sb t0, t1", in("t0") byte, in("t2") 0x10000000);
+        #[cfg(target_arch = "x86_64")]
+        asm!("out dx, al", in("dx") 0x03f8, in("al") byte);
     }
 }
