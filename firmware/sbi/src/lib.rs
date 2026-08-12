a77cc3e5babdf6834a3464f2691905ca9c159ae5
TaiseiIto <taisei1212@outlook.jp>
2026-08-12 22:36:03 +0900

    Add SBI ecall #5

--- firmware/sbi/src/lib.rs
+++ firmware/sbi/src/lib.rs
@@ -1,1 +1,40 @@
 #![no_std]
+
+use core::arch::asm;
+
+/// # References
+/// * [sbiret](https://docs.riscv.org/reference/sbi/v3.0/binary-encoding.html)
+pub struct Ret {
+    error: usize,
+    value: usize,
+}
+
+/// # References
+/// * [ECALL](https://docs.riscv.org/reference/sbi/v3.0/binary-encoding.html)
+pub fn ecall(
+    fid: usize,
+    eid: usize,
+    a0: usize,
+    a1: usize,
+    a2: usize,
+    a3: usize,
+    a4: usize,
+    a5: usize,
+) -> Ret {
+    let mut error: usize = a0;
+    let mut value: usize = a1;
+    unsafe {
+        asm!(
+            "ecall",
+            inout("a0") error,
+            inout("a1") value,
+            in("a2") a2,
+            in("a3") a3,
+            in("a4") a4,
+            in("a5") a5,
+            in("a6") fid,
+            in("a7") eid,
+        );
+    }
+    Ret { error, value }
+}
