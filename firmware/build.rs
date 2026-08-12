c0a128c495a76601a74477c369da85442866da47
TaiseiIto <taisei1212@outlook.jp>
2026-08-12 20:41:51 +0900

    Solve warning #5

--- firmware/build.rs
+++ firmware/build.rs
@@ -1,7 +1,7 @@
 use std::env::var;
 
 fn main() {
-    println!("cargo:rustc-check-cfg=cfg(firmware, values(\"open_sbi\",\"uefi\"))");
+    println!("cargo:rustc-check-cfg=cfg(firmware, values(\"open_sbi\",\"tfa\",\"uefi\"))");
     println!(
         "cargo:rustc-cfg=firmware=\"{}\"",
         match var("CARGO_CFG_TARGET_ARCH").unwrap().as_str() {
