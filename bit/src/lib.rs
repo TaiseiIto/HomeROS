d0f21ca230c4b985184e8062c247ed158521b7b8
TaiseiIto <taisei1212@outlook.jp>
2026-08-13 13:46:52 +0900

    Refactor

--- bit/src/lib.rs
+++ bit/src/lib.rs
@@ -2,5 +2,5 @@ use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
 pub fn field(attributes: TokenStream, item: TokenStream) -> TokenStream {
-    quote! {}.try_into().unwrap()
+    quote! {}.into()
 }
