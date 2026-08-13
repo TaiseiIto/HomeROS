cc1ca2c33a28ddcdc4cae754fa0743db2c21afa3
TaiseiIto <taisei1212@outlook.jp>
2026-08-13 16:58:12 +0900

    Analyze bit field struct #5

--- bit/src/lib.rs
+++ bit/src/lib.rs
@@ -1,7 +1,8 @@
 use {
     quote::quote,
     syn::{
-        ItemStruct,
+        Expr, ExprLit, Field, Fields, FieldsNamed, Ident, ItemStruct, Lit, LitInt, Path,
+        PathSegment, Type, TypeArray, TypePath, Visibility,
         parse::{Parse, ParseStream, Result},
         parse_macro_input,
     },
@@ -17,11 +18,100 @@ pub fn field(
 }
 
 struct Element {
-    name: Option<String>,
+    ident: Option<Ident>,
     bits: u8,
 }
 
+impl Element {
+    fn type2bits(ty: Type) -> u8 {
+        match ty {
+            Type::Array(TypeArray {
+                attrs,
+                bracket_token,
+                elem,
+                semi_token,
+                len,
+            }) => {
+                let unit: u8 = Self::type2bits(*elem);
+                let len: u8 = if let Expr::Lit(ExprLit {
+                    attrs,
+                    lit: Lit::Int(lit_int),
+                }) = len
+                {
+                    lit_int.base10_parse::<u8>().unwrap()
+                } else {
+                    panic!();
+                };
+                len * unit
+            }
+            Type::Path(TypePath {
+                attrs,
+                qself,
+                path:
+                    Path {
+                        leading_colon,
+                        segments,
+                    },
+            }) => {
+                let mut segments = segments.iter();
+                let segment: &PathSegment = segments.next().unwrap();
+                assert!(segments.next().is_none());
+                let PathSegment { ident, arguments } = segment;
+                assert!(ident == "bool");
+                1
+            }
+            _ => panic!(),
+        }
+    }
+}
+
+impl From<Field> for Element {
+    fn from(field: Field) -> Self {
+        if let Field {
+            attrs,
+            vis,
+            modifiers,
+            ident: Some(ident),
+            colon_token,
+            ty,
+            default,
+        } = field
+        {
+            let ident: Option<Ident> = (ident != "reserved").then_some(ident);
+            let bits: u8 = Self::type2bits(ty);
+            Self { ident, bits }
+        } else {
+            panic!();
+        }
+    }
+}
+
 struct Structure {
-    name: String,
+    vis: Visibility,
+    ident: Ident,
     elements: Vec<Element>,
+}
+
+impl From<ItemStruct> for Structure {
+    fn from(item_struct: ItemStruct) -> Self {
+        if let ItemStruct {
+            attrs,
+            vis,
+            struct_token,
+            ident,
+            generics,
+            fields: Fields::Named(FieldsNamed { brace_token, named }),
+            semi_token,
+        } = item_struct
+        {
+            let elements: Vec<Element> = named.into_iter().map(|field| field.into()).collect();
+            Self {
+                vis,
+                ident,
+                elements,
+            }
+        } else {
+            panic!();
+        }
+    }
 }
