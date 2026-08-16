65caaaa6a1669c71ce36a1b40f9f2f7960081b12
TaiseiIto <taisei1212@outlook.jp>
2026-08-16 16:33:04 +0900

    Register union syntax analysis #5

--- macro/io/src/overlap.rs
+++ macro/io/src/overlap.rs
@@ -1,15 +1,64 @@
-use {proc_macro2::TokenStream, quote::quote, syn::ItemUnion};
+use {
+    proc_macro2::TokenStream,
+    quote::quote,
+    syn::{Field, FieldsNamed, Ident, ItemUnion, Type, Visibility},
+};
 
-pub struct Registers {}
+pub struct Registers {
+    elements: Vec<Element>,
+    ident: Ident,
+    vis: Visibility,
+}
 
 impl From<ItemUnion> for Registers {
     fn from(item_union: ItemUnion) -> Self {
-        Self {}
+        let ItemUnion {
+            attrs: _,
+            vis,
+            union_token: _,
+            ident,
+            generics: _,
+            fields:
+                FieldsNamed {
+                    brace_token: _,
+                    named,
+                },
+        } = item_union;
+        let elements: Vec<Element> = named.into_iter().map(|field| field.into()).collect();
+        Self {
+            elements,
+            ident,
+            vis,
+        }
     }
 }
 
 impl From<Registers> for TokenStream {
     fn from(registers: Registers) -> Self {
         quote! {}
+    }
+}
+
+struct Element {
+    ident: Ident,
+    ty: Type,
+}
+
+impl From<Field> for Element {
+    fn from(field: Field) -> Self {
+        if let Field {
+            attrs: _,
+            vis: _,
+            modifiers: _,
+            ident: Some(ident),
+            colon_token: _,
+            ty,
+            default: _,
+        } = field
+        {
+            Self { ident, ty }
+        } else {
+            panic!();
+        }
     }
 }
