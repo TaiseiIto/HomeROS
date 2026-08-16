2b465f06382cba5674f271a9eb98d4d807e0be26
TaiseiIto <taisei1212@outlook.jp>
2026-08-16 20:35:08 +0900

    Add overlap register writer enum #5

--- macro/io/src/overlap.rs
+++ macro/io/src/overlap.rs
@@ -33,8 +33,7 @@ impl Registers {
     }
 
     fn reader_type_ident(&self) -> Ident {
-        let ident: &Ident = &self.ident;
-        Ident::new(&format!("{}Reader", ident), ident.span())
+        self.type_ident("Reader")
     }
 
     fn true_type(&self) -> TokenStream {
@@ -53,6 +52,33 @@ impl Registers {
                 #(#elements),*
             }
         }
+    }
+
+    fn type_ident(&self, suffix: &str) -> Ident {
+        let ident: &Ident = &self.ident;
+        Ident::new(&format!("{}{}", ident, suffix), ident.span())
+    }
+
+    fn writer_type(&self) -> TokenStream {
+        let Self {
+            elements,
+            ident,
+            vis,
+        } = self;
+        let writer_type: Ident = self.writer_type_ident();
+        let elements: Vec<TokenStream> = elements
+            .iter()
+            .map(|element| element.writer_declaration())
+            .collect();
+        quote! {
+            #vis enum #writer_type {
+                #(#elements),*
+            }
+        }
+    }
+
+    fn writer_type_ident(&self) -> Ident {
+        self.type_ident("Writer")
     }
 }
 
@@ -81,11 +107,13 @@ impl From<ItemUnion> for Registers {
 
 impl From<Registers> for TokenStream {
     fn from(registers: Registers) -> Self {
-        let true_type: TokenStream = registers.true_type();
         let reader_type: TokenStream = registers.reader_type();
+        let true_type: TokenStream = registers.true_type();
+        let writer_type: TokenStream = registers.writer_type();
         quote! {
-            #true_type
             #reader_type
+            #true_type
+            #writer_type
         }
     }
 }
@@ -96,15 +124,7 @@ struct Element {
 }
 
 impl Element {
-    fn reader_declaration(&self) -> TokenStream {
-        let ident: &Ident = &self.ident;
-        let reader_type_path: Path = self.reader_type_path();
-        quote! {
-            #ident: #reader_type_path
-        }
-    }
-
-    fn reader_type_path(&self) -> Path {
+    fn pretty_type_path(&self) -> Path {
         let Path {
             leading_colon,
             segments,
@@ -123,11 +143,38 @@ impl Element {
         }
     }
 
+    fn reader_declaration(&self) -> TokenStream {
+        let ident: &Ident = &self.ident;
+        let pretty_type_path: Path = self.pretty_type_path();
+        quote! {
+            #ident: #pretty_type_path
+        }
+    }
+
     fn true_declaration(&self) -> TokenStream {
         let Self { ident, type_path } = self;
         quote! {
             #ident: core::mem::ManuallyDrop<#type_path>
         }
+    }
+
+    fn writer_declaration(&self) -> TokenStream {
+        let writer: Ident = self.writer_ident();
+        let pretty_type_path: Path = self.pretty_type_path();
+        quote! {
+            #writer(#pretty_type_path)
+        }
+    }
+
+    fn writer_ident(&self) -> Ident {
+        let ident: &Ident = &self.ident;
+        let writer: String = ident
+            .to_string()
+            .to_uppercase()
+            .chars()
+            .filter(|c| c.is_ascii_alphanumeric())
+            .collect();
+        Ident::new(writer.as_str(), ident.span())
     }
 }
 
