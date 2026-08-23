dcf2b0ca1d38fae39a802760b3274a489f665d0d
TaiseiIto <taisei1212@outlook.jp>
2026-08-23 12:53:49 +0900

    Delete UART temporary function #5

--- device/uart/src/lib.rs
+++ device/uart/src/lib.rs
@@ -89,20 +89,6 @@ enum Parity {
 }
 
 /// # TODO
-/// This is an ad hoc implementation working on only QEMU.
-/// Implement RS232C completely.
-pub fn write_byte(byte: u8) {
-    unsafe {
-        #[cfg(target_arch = "aarch64")]
-        asm!("strb {byte:w}, [{address:x}]", byte = in(reg) byte, address = in(reg) 0x09000000);
-        #[cfg(target_arch = "riscv64")]
-        asm!("sb {byte}, ({address})", byte = in(reg) byte, address = in(reg) 0x10000000);
-        #[cfg(target_arch = "x86_64")]
-        asm!("out dx, al", in("dx") 0x03f8, in("al") byte);
-    }
-}
-
-/// # TODO
 /// This is not thread safe actualty.
 /// Make it thread safe.
 struct SyncOnceCell<T>(OnceCell<T>);
@@ -111,6 +97,7 @@ unsafe impl<T> Sync for SyncOnceCell<T> {}
 
 pub trait Driver {
     fn can_send_byte(&self) -> bool;
+
     fn initialize(
         &mut self,
         baud_rate: usize,
@@ -120,6 +107,7 @@ pub trait Driver {
         stop_bits: u8,
         word_bits: u8,
     );
+
     unsafe fn send_byte_unchecked(&mut self, data: u8);
 
     fn send_byte(&mut self, data: u8) {
--- firmware/src/lib.rs
+++ firmware/src/lib.rs
@@ -65,19 +65,18 @@ impl Global {
         self.write_fmt(arguments).unwrap();
     }
 
-    #[cfg(any(firmware = "sbi", firmware = "tfa"))]
+    #[cfg(firmware = "sbi")]
     fn write_byte(&self, byte: u8) {
-        #[cfg(firmware = "sbi")]
         sbi::console::putchar(byte);
-        #[cfg(firmware = "tfa")]
-        uart::write_byte(byte);
     }
 
     fn write_string(&self, string: &str) {
-        #[cfg(any(firmware = "sbi", firmware = "tfa"))]
+        #[cfg(firmware = "sbi")]
         for byte in string.bytes() {
             self.write_byte(byte);
         }
+        #[cfg(firmware = "tfa")]
+        uart::global_mut().write_str(string).unwrap();
         #[cfg(firmware = "uefi")]
         self.system_table.write(string);
     }
