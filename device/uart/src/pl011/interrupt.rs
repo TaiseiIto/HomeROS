54a9f6dde11806087d9a4e668ed4a4e19cbbd019
TaiseiIto <taisei1212@outlook.jp>
2026-08-24 23:42:32 +0900

    Refactor

--- device/uart/src/pl011/interrupt.rs
+++ device/uart/src/pl011/interrupt.rs
@@ -20,32 +20,3 @@ pub struct Register {
     overrun_error: bool,
     __: [bool; 21],
 }
-
-impl Register {
-    pub fn new(
-        ri_modem: bool,
-        cts_modem: bool,
-        dcd_modem: bool,
-        dsr_modem: bool,
-        receive: bool,
-        transmit: bool,
-        receive_timeout: bool,
-        framing_error: bool,
-        parity_error: bool,
-        break_erro: bool,
-        overrun_error: bool,
-    ) -> Self {
-        Self::default()
-            .update_ri_modem_bit(ri_modem)
-            .update_cts_modem_bit(cts_modem)
-            .update_dcd_modem_bit(dcd_modem)
-            .update_dsr_modem_bit(dsr_modem)
-            .update_receive_bit(receive)
-            .update_transmit_bit(transmit)
-            .update_receive_timeout_bit(receive_timeout)
-            .update_framing_error_bit(framing_error)
-            .update_parity_error_bit(parity_error)
-            .update_break_erro_bit(break_erro)
-            .update_overrun_error_bit(overrun_error)
-    }
-}
