7503bdf2e891e8020dc16ae7c3ea1d255e9607f8
TaiseiIto <taisei1212@outlook.jp>
2026-08-27 19:02:08 +0900

    Add spin guard #5

--- sync/src/spin.rs
+++ sync/src/spin.rs
@@ -6,6 +6,31 @@ use {
     },
 };
 
+pub struct Guard<'a, T>(&'a SpinLock<T>);
+
+impl<T> Deref for Guard<'_, T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        unsafe { &*self.0.value.get() }
+    }
+}
+
+impl<T> DerefMut for Guard<'_, T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe { &mut *self.0.value.get() }
+    }
+}
+
+impl<T> Drop for Guard<'_, T> {
+    fn drop(&mut self) {
+        self.0.unlock();
+    }
+}
+
+unsafe impl<T> Send for Guard<'_, T> where T: Send {}
+unsafe impl<T> Sync for Guard<'_, T> where T: Sync {}
+
 pub struct Lock<T> {
     locked: AtomicBool,
     value: UnsafeCell<T>,
@@ -19,11 +44,11 @@ impl Lock {
         }
     }
 
-    pub fn lock(&self) -> &mut T {
+    pub fn lock(&self) -> Guard<T> {
         while self.locked.swap(true, Ordering::Acquire) {
             pause();
         }
-        unsafe { &mut *self.value.get() }
+        Guard { lock: self }
     }
 
     pub fn unlock(&self) {
