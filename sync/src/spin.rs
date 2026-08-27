4d0b62cc01d7c6eb90b65080a74728b19b2eb661
TaiseiIto <taisei1212@outlook.jp>
2026-08-27 18:44:57 +0900

    Add spin lock #5

--- sync/src/spin.rs
+++ sync/src/spin.rs
@@ -1,26 +1,34 @@
 use {
     arch::pause,
-    core::sync::atomic::{AtomicBool, Ordering},
+    core::{
+        cell::UnsafeCell,
+        sync::atomic::{AtomicBool, Ordering},
+    },
 };
 
-pub struct Lock {
+pub struct Lock<T> {
     locked: AtomicBool,
+    value: UnsafeCell<T>,
 }
 
 impl Lock {
-    pub const fn new() -> Self {
+    pub const fn new(value: T) -> Self {
         Self {
             locked: AtomicBool::new(false),
+            value: UnsafeCell::new(value),
         }
     }
 
-    pub fn lock(&self) {
+    pub fn lock(&self) -> &mut T {
         while self.locked.swap(true, Ordering::Acquire) {
             pause();
         }
+        unsafe { &mut *self.value.get() }
     }
 
     pub fn unlock(&self) {
         self.locked.store(false, Ordering::Release);
     }
 }
+
+unsafe impl<T> Sync for SpinLock<T> where T: Send {}
