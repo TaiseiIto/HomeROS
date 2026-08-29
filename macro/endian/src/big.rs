15258e368ddf2b5c8822536c818971efdd593cb5
TaiseiIto <taisei1212@outlook.jp>
2026-08-29 18:45:22 +0900

    Analyze big endian structure #7

--- macro/endian/src/big.rs
+++ macro/endian/src/big.rs
@@ -1,15 +1,67 @@
-use {proc_macro2::TokenStream, quote::quote, syn::DeriveInput};
+use {
+    proc_macro2::TokenStream,
+    quote::quote,
+    syn::{Data, DataStruct, DeriveInput, Field, Fields, FieldsNamed, Ident, Type},
+};
 
-pub struct Structure {}
+pub struct Structure {
+    elements: Vec<Element>,
+    ident: Ident,
+}
 
 impl From<DeriveInput> for Structure {
     fn from(structure: DeriveInput) -> Self {
-        Self {}
+        if let DeriveInput {
+            attrs: _,
+            vis: _,
+            ident,
+            generics: _,
+            data:
+                Data::Struct(DataStruct {
+                    struct_token: _,
+                    fields:
+                        Fields::Named(FieldsNamed {
+                            brace_token: _,
+                            named,
+                        }),
+                    semi_token: _,
+                }),
+        } = structure
+        {
+            let elements: Vec<Element> = named.into_iter().map(|field| field.into()).collect();
+            Self { elements, ident }
+        } else {
+            unimplemented!();
+        }
     }
 }
 
 impl From<Structure> for TokenStream {
     fn from(structure: Structure) -> Self {
         quote! {}
+    }
+}
+
+struct Element {
+    ident: Ident,
+    ty: Type,
+}
+
+impl From<Field> for Element {
+    fn from(field: Field) -> Self {
+        if let Field {
+            attrs: _,
+            vis: _,
+            modifiers: _,
+            ident: Some(ident),
+            colon_token: _,
+            ty,
+            default: _,
+        } = field
+        {
+            Self { ident, ty }
+        } else {
+            unimplemented!();
+        }
     }
 }
