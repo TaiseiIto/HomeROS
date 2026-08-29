4e52fa5393d085e942613007eea98d4608727d34
TaiseiIto <taisei1212@outlook.jp>
2026-08-29 21:31:43 +0900

    Implement UEFI table header debug trait

--- firmware/uefi/src/table.rs
+++ firmware/uefi/src/table.rs
@@ -1,6 +1,7 @@
+use core::fmt::{Debug, Formatter, Result};
+
 /// # References
 /// * [EFI_TABLE_HEADER](https://uefi.org/specs/UEFI/2.11/04_EFI_System_Table.html#id4)
-#[derive(Debug)]
 #[repr(C)]
 pub struct Header {
     signature: u64,
@@ -8,4 +9,19 @@ pub struct Header {
     header_size: u32,
     crc32: u32,
     __: u32,
+}
+
+impl Debug for Header {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        formatter
+            .debug_struct("Header")
+            .field(
+                "signature",
+                &str::from_utf8(self.signature.to_le_bytes().as_slice()).unwrap(),
+            )
+            .field("revision", &self.revision)
+            .field("header_size", &self.header_size)
+            .field("crc32", &self.crc32)
+            .finish()
+    }
 }
