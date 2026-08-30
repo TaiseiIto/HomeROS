4669e55212077bed4cb9b84388084e6bdda52c30
TaiseiIto <taisei1212@outlook.jp>
2026-08-30 09:09:41 +0900

    Print device tree structures #7

--- device/tree/src/lib.rs
+++ device/tree/src/lib.rs
@@ -1,7 +1,9 @@
+#![feature(option_array_transpose)]
 #![no_std]
 
 use core::{
     fmt::{Debug, Formatter, Result},
+    mem::size_of,
     slice::from_raw_parts,
 };
 
@@ -23,6 +25,24 @@ pub struct Header {
 }
 
 impl Header {
+    fn iter(&self) -> StructureIterator {
+        StructureIterator {
+            header: self,
+            structure_offset: 0,
+        }
+    }
+
+    fn string(&self, offset: usize) -> &str {
+        let strings_bytes: &[u8] = self.strings_bytes();
+        let strings_bytes_len: usize = strings_bytes.len();
+        let end: usize = (offset..strings_bytes_len)
+            .take_while(|offset| strings_bytes.get(*offset).is_some_and(|byte| *byte != 0x00))
+            .max()
+            .map(|last_index| last_index + 1)
+            .unwrap_or(offset);
+        str::from_utf8(&strings_bytes[offset..end]).unwrap()
+    }
+
     fn strings_bytes(&self) -> &[u8] {
         let offset: usize = self.read_off_dt_strings() as usize;
         let size: usize = self.read_size_dt_strings() as usize;
@@ -46,12 +66,100 @@ impl Debug for Header {
             .debug_struct("Header")
             .field("magic", &self.read_magic())
             .field("totalsize", &self.read_totalsize())
-            .field("structure", &self.structure_bytes())
-            .field("strings", &self.strings_bytes())
+            .field("structures", &self.iter())
             .field("off_mem_rsvmap", &self.read_off_mem_rsvmap())
             .field("version", &self.read_version())
             .field("last_comp_version", &self.read_last_comp_version())
             .field("boot_cpuid_phys", &self.read_boot_cpuid_phys())
             .finish()
+    }
+}
+
+#[derive(Debug)]
+enum Structure<'a> {
+    BeginNode { name: &'a str },
+    EndNode,
+    Property { name: &'a str, data: &'a [u8] },
+    Nop,
+    End,
+}
+
+#[derive(Clone)]
+struct StructureIterator<'a> {
+    header: &'a Header,
+    structure_offset: usize,
+}
+
+impl StructureIterator<'_> {
+    fn take_byte(&mut self) -> Option<u8> {
+        let byte: Option<u8> = self
+            .header
+            .structure_bytes()
+            .get(self.structure_offset)
+            .copied();
+        self.structure_offset += 1;
+        byte
+    }
+
+    fn take_word(&mut self) -> Option<u32> {
+        [
+            self.take_byte(),
+            self.take_byte(),
+            self.take_byte(),
+            self.take_byte(),
+        ]
+        .transpose()
+        .map(u32::from_be_bytes)
+    }
+}
+
+impl Debug for StructureIterator<'_> {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        formatter.debug_list().entries(self.clone()).finish()
+    }
+}
+
+impl<'a> Iterator for StructureIterator<'a> {
+    type Item = Structure<'a>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.take_word().map(|token| match token {
+            0x00000001 => {
+                let Self {
+                    header,
+                    structure_offset,
+                } = self;
+                let remaining_bytes: &[u8] = &header.structure_bytes()[*structure_offset..];
+                let name_size: usize = remaining_bytes
+                    .iter()
+                    .take_while(|byte| **byte != 0x00)
+                    .enumerate()
+                    .map(|(index, _)| index + 1)
+                    .max()
+                    .unwrap_or(0);
+                let name: &str = str::from_utf8(&remaining_bytes[..name_size]).unwrap();
+                *structure_offset += name_size + size_of::<u32>() - 1;
+                *structure_offset &= !size_of::<u32>();
+                Self::Item::BeginNode { name }
+            }
+            0x00000002 => Self::Item::EndNode,
+            0x00000003 => {
+                let length: usize = self.take_word().unwrap() as usize;
+                let name_offset: usize = self.take_word().unwrap() as usize;
+                let Self {
+                    header,
+                    structure_offset,
+                } = self;
+                let name: &str = header.string(name_offset);
+                let remaining_bytes: &[u8] = &header.structure_bytes()[*structure_offset..];
+                let data: &[u8] = &remaining_bytes[..length];
+                *structure_offset += length + size_of::<u32>() - 1;
+                *structure_offset &= !size_of::<u32>();
+                Self::Item::Property { name, data }
+            }
+            0x00000004 => Self::Item::Nop,
+            0x00000009 => Self::Item::End,
+            _ => unreachable!(),
+        })
     }
 }
