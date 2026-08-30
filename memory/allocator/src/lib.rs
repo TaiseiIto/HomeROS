04c9be80be8e5fbb4b0924b5ec4066ffa36f8ef1
TaiseiIto <taisei1212@outlook.jp>
2026-08-30 18:42:22 +0900

    Add memory allocator #7

--- memory/allocator/src/lib.rs
+++ memory/allocator/src/lib.rs
@@ -1,1 +1,38 @@
 #![no_std]
+
+use {
+    alloc::alloc::Layout,
+    core::{alloc::GlobalAlloc, cell::UnsafeCell},
+    sync::spin::Lock,
+};
+
+#[global_allocator]
+static GLOBAL: Global = Global::new();
+
+struct Global(Lock<UnsafeCell<Allocator>>);
+
+impl Global {
+    const fn new() -> Self {
+        Self(Lock::new(UnsafeCell::new(Allocator::new())))
+    }
+}
+
+unsafe impl GlobalAlloc for Global {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        unimplemented!();
+    }
+
+    unsafe fn dealloc(&self, address: *mut u8, layout: Layout) {
+        unimplemented!();
+    }
+}
+
+enum Allocator {
+    Uninitialized,
+}
+
+impl Allocator {
+    const fn new() -> Self {
+        Self::Uninitialized
+    }
+}
