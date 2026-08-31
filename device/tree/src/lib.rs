e91c766e2c8f493ada25852c6fe4887787a123ee
TaiseiIto <taisei1212@outlook.jp>
2026-08-31 22:11:16 +0900

    Read device tree interrupts extended property #7

--- device/tree/src/lib.rs
+++ device/tree/src/lib.rs
@@ -149,6 +149,9 @@ enum Property {
     Interrupts(Vec<u32>),
     /// # References
     /// * [Devicetree Specification](https://github.com/devicetree-org/devicetree-specification/releases/download/v0.4/devicetree-specification-v0.4.pdf) 2.4.1 Properties for Interrupt Generating Devices
+    InterruptsExtended(Vec<u32>),
+    /// # References
+    /// * [Devicetree Specification](https://github.com/devicetree-org/devicetree-specification/releases/download/v0.4/devicetree-specification-v0.4.pdf) 2.4.1 Properties for Interrupt Generating Devices
     InterruptParent(u32),
     /// # References
     /// * [Devicetree Specification](https://github.com/devicetree-org/devicetree-specification/releases/download/v0.4/devicetree-specification-v0.4.pdf) 2.3.2 model
@@ -223,8 +226,9 @@ impl Property {
             "dma-coherent" => Self::DmaCoherent,
             "dma-noncoherent" => Self::DmaNonCoherent,
             "dma-ranges" => Self::DmaRanges(Self::data2u32s(data)),
-            "interrupts" => Self::Interrupts(Self::data2u32s(data)),
             "interrupt-parent" => Self::InterruptParent(Self::data2u32(data)),
+            "interrupts" => Self::Interrupts(Self::data2u32s(data)),
+            "interrupts-extended" => Self::InterruptsExtended(Self::data2u32s(data)),
             "model" => Self::Model(Self::data2string(data)),
             "name" => Self::Name(Self::data2string(data)),
             "no-map" => Self::NoMap,
