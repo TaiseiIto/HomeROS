841f1e1f3facb3e8b1ea1572725646cb74dc520b
TaiseiIto <taisei1212@outlook.jp>
2026-09-04 23:21:44 +0900

    Implement Debug trait for device tree reg property #7

--- device/tree/src/property/reg.rs
+++ device/tree/src/property/reg.rs
@@ -1,11 +1,22 @@
-use alloc::vec::Vec;
+use {
+    alloc::vec::Vec,
+    core::fmt::{Debug, Formatter, Result},
+};
 
 /// # References
 /// * [Devicetree Specification](https://github.com/devicetree-org/devicetree-specification/releases/download/v0.4/devicetree-specification-v0.4.pdf) 2.3.6 reg
-#[derive(Debug)]
 pub enum Reg {
     Raw(Vec<u32>),
     Pretty(Vec<Pair>),
+}
+
+impl Debug for Reg {
+    fn fmt(&self, formatter: &mut Formatter<'_>) -> Result {
+        match self {
+            Self::Raw(words) => formatter.debug_list().entries(words).finish(),
+            Self::Pretty(pairs) => formatter.debug_list().entries(pairs).finish(),
+        }
+    }
 }
 
 #[derive(Debug)]
