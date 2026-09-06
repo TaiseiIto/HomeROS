e8c59ef2a6b470be5ed7b0a747dfb9b805c839cc
TaiseiIto <taisei1212@outlook.jp>
2026-09-07 00:33:48 +0900

    Test memory retion subtraction #7

--- memory/src/lib.rs
+++ memory/src/lib.rs
@@ -204,16 +204,12 @@ impl TryFrom<Range<usize>> for Regions {
     }
 }
 
-impl TryFrom<&[Range<usize>]> for Regions {
-    type Error = ();
-
-    fn try_from(ranges: &[Range<usize>]) -> Result<Self, Self::Error> {
+impl From<&[Range<usize>]> for Regions {
+    fn from(ranges: &[Range<usize>]) -> Self {
         ranges
             .iter()
-            .map(|range| range.clone().try_into().ok())
-            .collect::<Option<Vec<Self>>>()
-            .map(|regions| regions.into_iter().sum())
-            .ok_or(())
+            .filter_map(|range| range.clone().try_into().ok())
+            .sum()
     }
 }
 
@@ -251,8 +247,36 @@ mod tests {
 
     #[test]
     fn add_regions() {
-        let a: Regions = [(0..1), (2..3), (4..5)].as_slice().try_into().unwrap();
-        let b: Regions = [(1..2), (3..4), (5..6)].as_slice().try_into().unwrap();
+        let a: Regions = [(0..1), (2..3), (4..5)].as_slice().into();
+        let b: Regions = [(1..2), (3..4), (5..6)].as_slice().into();
         assert_eq!(a + b, (0..6).try_into().unwrap());
+    }
+
+    #[test]
+    fn subtract_region() {
+        let a: Region = (0..1).try_into().unwrap();
+        let b: Region = (2..3).try_into().unwrap();
+        assert_eq!(a.clone() - b.clone(), a.clone().into());
+        assert_eq!(b.clone() - a.clone(), b.clone().into());
+        let a: Region = (0..1).try_into().unwrap();
+        let b: Region = (1..2).try_into().unwrap();
+        assert_eq!(a.clone() - b.clone(), a.clone().into());
+        assert_eq!(b.clone() - a.clone(), b.clone().into());
+        let a: Region = (0..2).try_into().unwrap();
+        let b: Region = (1..3).try_into().unwrap();
+        assert_eq!(a.clone() - b.clone(), (0..1).try_into().unwrap());
+        assert_eq!(b.clone() - a.clone(), (2..3).try_into().unwrap());
+        let a: Region = (0..2).try_into().unwrap();
+        let b: Region = (0..1).try_into().unwrap();
+        assert_eq!(a.clone() - b.clone(), (1..2).try_into().unwrap());
+        assert_eq!(b.clone() - a.clone(), Regions::default());
+        let a: Region = (0..2).try_into().unwrap();
+        let b: Region = (1..2).try_into().unwrap();
+        assert_eq!(a.clone() - b.clone(), (0..1).try_into().unwrap());
+        assert_eq!(b.clone() - a.clone(), Regions::default());
+        let a: Region = (0..3).try_into().unwrap();
+        let b: Region = (1..2).try_into().unwrap();
+        assert_eq!(a.clone() - b.clone(), [(0..1), (2..3)].as_slice().into());
+        assert_eq!(b.clone() - a.clone(), Regions::default());
     }
 }
