527049d98c6a148d745bb8ecfc3ad41d07b0cc03
TaiseiIto <taisei1212@outlook.jp>
2026-09-09 21:28:34 +0900

    Define regex nonterminal symbols #7

--- tool/regex/src/lib.rs
+++ tool/regex/src/lib.rs
@@ -1,1 +1,389 @@
 #![no_std]
+
+extern crate alloc;
+
+use alloc::boxed::Box;
+
+struct Expression(Term, Option<(VerticalBar, Box<Expression>)>);
+struct Term(Power, Option<Box<Term>>);
+struct Power(Base, Option<Exponent>);
+enum Base {
+    Character(Character),
+    Expression(LeftParenthesis, Box<Expression>, RightParenthesis),
+    Set(LeftBracket, Option<Circumflex>, Set, RightBracket),
+}
+enum Character {
+    Circumflex(Circumflex),
+    Dollar(Dollar),
+    EscapedCharacter(Backslash, EscapedCharacter),
+    Period(Period),
+    UnescapedCharacter(UnescapedCharacter),
+}
+enum EscapedCharacter {
+    Asterisk(Asterisk),
+    Backslash(Backslash),
+    Circumflex(Circumflex),
+    Dollar(Dollar),
+    LeftBrace(LeftBrace),
+    LeftBracket(LeftBracket),
+    LeftParenthesis(LeftParenthesis),
+    LowerD(LowerD),
+    LowerF(LowerF),
+    LowerL(LowerL),
+    LowerN(LowerN),
+    LowerR(LowerR),
+    LowerS(LowerS),
+    LowerT(LowerT),
+    LowerU(LowerU),
+    LowerW(LowerW),
+    LowerX(LowerX, Byte),
+    Period(Period),
+    Plus(Plus),
+    Question(Question),
+    RightBrace(RightBrace),
+    RightBracket(RightBracket),
+    RightParenthesis(RightParenthesis),
+    Slash(Slash),
+    UpperD(UpperD),
+    UpperL(UpperL),
+    UpperS(UpperS),
+    UpperU(UpperU),
+    UpperW(UpperW),
+    VerticalBar(VerticalBar),
+}
+struct Byte([Hexadecimal; 2]);
+enum Hexadecimal {
+    Zero(Zero),
+    One(One),
+    Two(Two),
+    Three(Three),
+    Four(Four),
+    Five(Five),
+    Six(Six),
+    Seven(Seven),
+    Eight(Eight),
+    Nine(Nine),
+    UpperA(UpperA),
+    UpperB(UpperB),
+    UpperC(UpperC),
+    UpperD(UpperD),
+    UpperE(UpperE),
+    UpperF(UpperF),
+}
+enum UnescapedCharacter {
+    Zero(Zero),
+    One(One),
+    Two(Two),
+    Three(Three),
+    Four(Four),
+    Five(Five),
+    Six(Six),
+    Seven(Seven),
+    Eight(Eight),
+    Nine(Nine),
+    LowerA(LowerA),
+    LowerB(LowerB),
+    LowerC(LowerC),
+    LowerD(LowerD),
+    LowerE(LowerE),
+    LowerF(LowerF),
+    LowerG(LowerG),
+    LowerH(LowerH),
+    LowerI(LowerI),
+    LowerJ(LowerJ),
+    LowerK(LowerK),
+    LowerL(LowerL),
+    LowerM(LowerM),
+    LowerN(LowerN),
+    LowerO(LowerO),
+    LowerP(LowerP),
+    LowerQ(LowerQ),
+    LowerR(LowerR),
+    LowerS(LowerS),
+    LowerT(LowerT),
+    LowerU(LowerU),
+    LowerV(LowerV),
+    LowerW(LowerW),
+    LowerX(LowerX),
+    LowerY(LowerY),
+    LowerZ(LowerZ),
+    UpperA(UpperA),
+    UpperB(UpperB),
+    UpperC(UpperC),
+    UpperD(UpperD),
+    UpperE(UpperE),
+    UpperF(UpperF),
+    UpperG(UpperG),
+    UpperH(UpperH),
+    UpperI(UpperI),
+    UpperJ(UpperJ),
+    UpperK(UpperK),
+    UpperL(UpperL),
+    UpperM(UpperM),
+    UpperN(UpperN),
+    UpperO(UpperO),
+    UpperP(UpperP),
+    UpperQ(UpperQ),
+    UpperR(UpperR),
+    UpperS(UpperS),
+    UpperT(UpperT),
+    UpperU(UpperU),
+    UpperV(UpperV),
+    UpperW(UpperW),
+    UpperX(UpperX),
+    UpperY(UpperY),
+    UpperZ(UpperZ),
+    Ampersand(Ampersand),
+    Apostrophe(Apostrophe),
+    At(At),
+    Colon(Colon),
+    Comma(Comma),
+    Equal(Equal),
+    Exclamation(Exclamation),
+    Grave(Grave),
+    GreaterThan(GreaterThan),
+    Hash(Hash),
+    Hyphen(Hyphen),
+    LessThan(LessThan),
+    Percent(Percent),
+    Quotation(Quotation),
+    Semicolon(Semicolon),
+    Tilde(Tilde),
+    Underscore(Underscore),
+}
+struct Set(Range, Option<Box<Set>>);
+struct Range(Element, Option<(Hyphen, Element)>);
+enum Element {
+    EscapedElement(Backslash, EscapedElement),
+    UnescapedElement(UnescapedElement),
+}
+enum EscapedElement {
+    Asterisk(Asterisk),
+    Backslash(Backslash),
+    Circumflex(Circumflex),
+    Dollar(Dollar),
+    Hyphen(Hyphen),
+    LeftBrace(LeftBrace),
+    LeftBracket(LeftBracket),
+    LeftParenthesis(LeftParenthesis),
+    LowerD(LowerD),
+    LowerF(LowerF),
+    LowerL(LowerL),
+    LowerN(LowerN),
+    LowerR(LowerR),
+    LowerS(LowerS),
+    LowerT(LowerT),
+    LowerU(LowerU),
+    LowerW(LowerW),
+    LowerX(LowerX, Byte),
+    Period(Period),
+    Plus(Plus),
+    Question(Question),
+    RightBrace(RightBrace),
+    RightBracket(RightBracket),
+    RightParenthesis(RightParenthesis),
+    Slash(Slash),
+    UpperD(UpperD),
+    UpperL(UpperL),
+    UpperS(UpperS),
+    UpperU(UpperU),
+    UpperW(UpperW),
+    VerticalBar(VerticalBar),
+}
+enum UnescapedElement {
+    Zero(Zero),
+    One(One),
+    Two(Two),
+    Three(Three),
+    Four(Four),
+    Five(Five),
+    Six(Six),
+    Seven(Seven),
+    Eight(Eight),
+    Nine(Nine),
+    LowerA(LowerA),
+    LowerB(LowerB),
+    LowerC(LowerC),
+    LowerD(LowerD),
+    LowerE(LowerE),
+    LowerF(LowerF),
+    LowerG(LowerG),
+    LowerH(LowerH),
+    LowerI(LowerI),
+    LowerJ(LowerJ),
+    LowerK(LowerK),
+    LowerL(LowerL),
+    LowerM(LowerM),
+    LowerN(LowerN),
+    LowerO(LowerO),
+    LowerP(LowerP),
+    LowerQ(LowerQ),
+    LowerR(LowerR),
+    LowerS(LowerS),
+    LowerT(LowerT),
+    LowerU(LowerU),
+    LowerV(LowerV),
+    LowerW(LowerW),
+    LowerX(LowerX),
+    LowerY(LowerY),
+    LowerZ(LowerZ),
+    UpperA(UpperA),
+    UpperB(UpperB),
+    UpperC(UpperC),
+    UpperD(UpperD),
+    UpperE(UpperE),
+    UpperF(UpperF),
+    UpperG(UpperG),
+    UpperH(UpperH),
+    UpperI(UpperI),
+    UpperJ(UpperJ),
+    UpperK(UpperK),
+    UpperL(UpperL),
+    UpperM(UpperM),
+    UpperN(UpperN),
+    UpperO(UpperO),
+    UpperP(UpperP),
+    UpperQ(UpperQ),
+    UpperR(UpperR),
+    UpperS(UpperS),
+    UpperT(UpperT),
+    UpperU(UpperU),
+    UpperV(UpperV),
+    UpperW(UpperW),
+    UpperX(UpperX),
+    UpperY(UpperY),
+    UpperZ(UpperZ),
+    Ampersand(Ampersand),
+    Apostrophe(Apostrophe),
+    At(At),
+    Colon(Colon),
+    Comma(Comma),
+    Equal(Equal),
+    Exclamation(Exclamation),
+    Grave(Grave),
+    GreaterThan(GreaterThan),
+    Hash(Hash),
+    LessThan(LessThan),
+    Percent(Percent),
+    Quotation(Quotation),
+    Semicolon(Semicolon),
+    Tilde(Tilde),
+    Underscore(Underscore),
+}
+enum Exponent {
+    Asterisk(Asterisk),
+    Plus(Plus),
+    Question(Question),
+    Range(
+        LeftBrace,
+        Number,
+        Option<(Comma, Option<Number>)>,
+        RightBrace,
+    ),
+}
+struct Number(Digit, Option<Box<Number>>);
+enum Digit {
+    One(One),
+    Two(Two),
+    Three(Three),
+    Four(Four),
+    Five(Five),
+    Six(Six),
+    Seven(Seven),
+    Eight(Eight),
+    Nine(Nine),
+}
+struct Zero;
+struct One;
+struct Two;
+struct Three;
+struct Four;
+struct Five;
+struct Six;
+struct Seven;
+struct Eight;
+struct Nine;
+struct LowerA;
+struct LowerB;
+struct LowerC;
+struct LowerD;
+struct LowerE;
+struct LowerF;
+struct LowerG;
+struct LowerH;
+struct LowerI;
+struct LowerJ;
+struct LowerK;
+struct LowerL;
+struct LowerM;
+struct LowerN;
+struct LowerO;
+struct LowerP;
+struct LowerQ;
+struct LowerR;
+struct LowerS;
+struct LowerT;
+struct LowerU;
+struct LowerV;
+struct LowerW;
+struct LowerX;
+struct LowerY;
+struct LowerZ;
+struct UpperA;
+struct UpperB;
+struct UpperC;
+struct UpperD;
+struct UpperE;
+struct UpperF;
+struct UpperG;
+struct UpperH;
+struct UpperI;
+struct UpperJ;
+struct UpperK;
+struct UpperL;
+struct UpperM;
+struct UpperN;
+struct UpperO;
+struct UpperP;
+struct UpperQ;
+struct UpperR;
+struct UpperS;
+struct UpperT;
+struct UpperU;
+struct UpperV;
+struct UpperW;
+struct UpperX;
+struct UpperY;
+struct UpperZ;
+struct Ampersand;
+struct Apostrophe;
+struct Asterisk;
+struct At;
+struct Backslash;
+struct Circumflex;
+struct Colon;
+struct Comma;
+struct Dollar;
+struct Equal;
+struct Exclamation;
+struct Grave;
+struct GreaterThan;
+struct Hash;
+struct Hyphen;
+struct LeftBrace;
+struct LeftBracket;
+struct LeftParenthesis;
+struct LessThan;
+struct Percent;
+struct Period;
+struct Plus;
+struct Question;
+struct Quotation;
+struct RightBrace;
+struct RightBracket;
+struct RightParenthesis;
+struct Semicolon;
+struct Slash;
+struct Tilde;
+struct Underscore;
+struct VerticalBar;
