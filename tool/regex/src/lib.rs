07640068d552466e6e6d0bf7ed30876a38e5eec8
TaiseiIto <taisei1212@outlook.jp>
2026-09-09 22:21:22 +0900

    Add parser generator #7

--- tool/regex/src/lib.rs
+++ tool/regex/src/lib.rs
@@ -332,280 +332,373 @@ enum Digit {
 struct Zero;
 
 #[derive(Parser)]
+#[terminal = '1']
 struct One;
 
 #[derive(Parser)]
+#[terminal = '2']
 struct Two;
 
 #[derive(Parser)]
+#[terminal = '3']
 struct Three;
 
 #[derive(Parser)]
+#[terminal = '4']
 struct Four;
 
 #[derive(Parser)]
+#[terminal = '5']
 struct Five;
 
 #[derive(Parser)]
+#[terminal = '6']
 struct Six;
 
 #[derive(Parser)]
+#[terminal = '7']
 struct Seven;
 
 #[derive(Parser)]
+#[terminal = '8']
 struct Eight;
 
 #[derive(Parser)]
+#[terminal = '9']
 struct Nine;
 
 #[derive(Parser)]
+#[terminal = 'a']
 struct LowerA;
 
 #[derive(Parser)]
+#[terminal = 'b']
 struct LowerB;
 
 #[derive(Parser)]
+#[terminal = 'c']
 struct LowerC;
 
 #[derive(Parser)]
+#[terminal = 'd']
 struct LowerD;
 
 #[derive(Parser)]
+#[terminal = 'e']
 struct LowerE;
 
 #[derive(Parser)]
+#[terminal = 'f']
 struct LowerF;
 
 #[derive(Parser)]
+#[terminal = 'g']
 struct LowerG;
 
 #[derive(Parser)]
+#[terminal = 'h']
 struct LowerH;
 
 #[derive(Parser)]
+#[terminal = 'i']
 struct LowerI;
 
 #[derive(Parser)]
+#[terminal = 'j']
 struct LowerJ;
 
 #[derive(Parser)]
+#[terminal = 'k']
 struct LowerK;
 
 #[derive(Parser)]
+#[terminal = 'l']
 struct LowerL;
 
 #[derive(Parser)]
+#[terminal = 'm']
 struct LowerM;
 
 #[derive(Parser)]
+#[terminal = 'n']
 struct LowerN;
 
 #[derive(Parser)]
+#[terminal = 'o']
 struct LowerO;
 
 #[derive(Parser)]
+#[terminal = 'p']
 struct LowerP;
 
 #[derive(Parser)]
+#[terminal = 'q']
 struct LowerQ;
 
 #[derive(Parser)]
+#[terminal = 'r']
 struct LowerR;
 
 #[derive(Parser)]
+#[terminal = 's']
 struct LowerS;
 
 #[derive(Parser)]
+#[terminal = 't']
 struct LowerT;
 
 #[derive(Parser)]
+#[terminal = 'u']
 struct LowerU;
 
 #[derive(Parser)]
+#[terminal = 'v']
 struct LowerV;
 
 #[derive(Parser)]
+#[terminal = 'w']
 struct LowerW;
 
 #[derive(Parser)]
+#[terminal = 'x']
 struct LowerX;
 
 #[derive(Parser)]
+#[terminal = 'y']
 struct LowerY;
 
 #[derive(Parser)]
+#[terminal = 'z']
 struct LowerZ;
 
 #[derive(Parser)]
+#[terminal = 'A']
 struct UpperA;
 
 #[derive(Parser)]
+#[terminal = 'B']
 struct UpperB;
 
 #[derive(Parser)]
+#[terminal = 'C']
 struct UpperC;
 
 #[derive(Parser)]
+#[terminal = 'D']
 struct UpperD;
 
 #[derive(Parser)]
+#[terminal = 'E']
 struct UpperE;
 
 #[derive(Parser)]
+#[terminal = 'F']
 struct UpperF;
 
 #[derive(Parser)]
+#[terminal = 'G']
 struct UpperG;
 
 #[derive(Parser)]
+#[terminal = 'H']
 struct UpperH;
 
 #[derive(Parser)]
+#[terminal = 'I']
 struct UpperI;
 
 #[derive(Parser)]
+#[terminal = 'J']
 struct UpperJ;
 
 #[derive(Parser)]
+#[terminal = 'K']
 struct UpperK;
 
 #[derive(Parser)]
+#[terminal = 'L']
 struct UpperL;
 
 #[derive(Parser)]
+#[terminal = 'M']
 struct UpperM;
 
 #[derive(Parser)]
+#[terminal = 'N']
 struct UpperN;
 
 #[derive(Parser)]
+#[terminal = 'O']
 struct UpperO;
 
 #[derive(Parser)]
+#[terminal = 'P']
 struct UpperP;
 
 #[derive(Parser)]
+#[terminal = 'Q']
 struct UpperQ;
 
 #[derive(Parser)]
+#[terminal = 'R']
 struct UpperR;
 
 #[derive(Parser)]
+#[terminal = 'S']
 struct UpperS;
 
 #[derive(Parser)]
+#[terminal = 'T']
 struct UpperT;
 
 #[derive(Parser)]
+#[terminal = 'U']
 struct UpperU;
 
 #[derive(Parser)]
+#[terminal = 'V']
 struct UpperV;
 
 #[derive(Parser)]
+#[terminal = 'W']
 struct UpperW;
 
 #[derive(Parser)]
+#[terminal = 'X']
 struct UpperX;
 
 #[derive(Parser)]
+#[terminal = 'Y']
 struct UpperY;
 
 #[derive(Parser)]
+#[terminal = 'Z']
 struct UpperZ;
 
 #[derive(Parser)]
+#[terminal = '&']
 struct Ampersand;
 
 #[derive(Parser)]
+#[terminal = '\'']
 struct Apostrophe;
 
 #[derive(Parser)]
+#[terminal = '*']
 struct Asterisk;
 
 #[derive(Parser)]
+#[terminal = '@']
 struct At;
 
 #[derive(Parser)]
+#[terminal = '\\']
 struct Backslash;
 
 #[derive(Parser)]
+#[terminal = '^']
 struct Circumflex;
 
 #[derive(Parser)]
+#[terminal = ':']
 struct Colon;
 
 #[derive(Parser)]
+#[terminal = ',']
 struct Comma;
 
 #[derive(Parser)]
+#[terminal = '$']
 struct Dollar;
 
 #[derive(Parser)]
+#[terminal = '=']
 struct Equal;
 
 #[derive(Parser)]
+#[terminal = '!']
 struct Exclamation;
 
 #[derive(Parser)]
+#[terminal = '`']
 struct Grave;
 
 #[derive(Parser)]
+#[terminal = '>']
 struct GreaterThan;
 
 #[derive(Parser)]
+#[terminal = '#']
 struct Hash;
 
 #[derive(Parser)]
+#[terminal = '-']
 struct Hyphen;
 
 #[derive(Parser)]
+#[terminal = '{']
 struct LeftBrace;
 
 #[derive(Parser)]
+#[terminal = '[']
 struct LeftBracket;
 
 #[derive(Parser)]
+#[terminal = '(']
 struct LeftParenthesis;
 
 #[derive(Parser)]
+#[terminal = '<']
 struct LessThan;
 
 #[derive(Parser)]
+#[terminal = '%']
 struct Percent;
 
 #[derive(Parser)]
+#[terminal = '.']
 struct Period;
 
 #[derive(Parser)]
+#[terminal = '+']
 struct Plus;
 
 #[derive(Parser)]
+#[terminal = '?']
 struct Question;
 
 #[derive(Parser)]
+#[terminal = '"']
 struct Quotation;
 
 #[derive(Parser)]
+#[terminal = '}']
 struct RightBrace;
 
 #[derive(Parser)]
+#[terminal = ']']
 struct RightBracket;
 
 #[derive(Parser)]
+#[terminal = ')']
 struct RightParenthesis;
 
 #[derive(Parser)]
+#[terminal = ';']
 struct Semicolon;
 
 #[derive(Parser)]
+#[terminal = '/']
 struct Slash;
 
 #[derive(Parser)]
+#[terminal = '~']
 struct Tilde;
 
 #[derive(Parser)]
+#[terminal = '_']
 struct Underscore;
 
 #[derive(Parser)]
+#[terminal = '|']
 struct VerticalBar;
