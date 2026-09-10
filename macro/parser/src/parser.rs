6b4634c88e06699581384d2920871233ac12b6a5
TaiseiIto <taisei1212@outlook.jp>
2026-09-10 19:07:23 +0900

    Parse symbol #7

--- macro/parser/src/parser.rs
+++ macro/parser/src/parser.rs
@@ -1,10 +1,23 @@
-use {proc_macro2::TokenStream, quote::quote, syn::DeriveInput};
+use {
+    proc_macro2::TokenStream,
+    quote::quote,
+    syn::{DeriveInput, Ident},
+};
 
-pub struct Symbol;
+pub struct Symbol {
+    name: Ident,
+}
 
 impl From<DeriveInput> for Symbol {
     fn from(symbol: DeriveInput) -> Self {
-        Self
+        let DeriveInput {
+            attrs,
+            vis,
+            ident,
+            generics,
+            data,
+        } = symbol;
+        Self { name: ident }
     }
 }
 
