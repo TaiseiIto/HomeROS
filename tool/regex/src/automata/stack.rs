8ff4ca8047f8048034d57eca4810a8cad8fb3315
TaiseiIto <taisei1212@outlook.jp>
2026-09-13 21:16:02 +0900

    Define regex prosessing phase #7

--- tool/regex/src/automata/stack.rs
+++ tool/regex/src/automata/stack.rs
@@ -13,22 +13,16 @@ impl<'a> From<&'a Automata> for Stack<'a> {
 
 struct Frame<'a> {
     automata: &'a Automata,
-    phase: Phase,
+    progress: Progress,
 }
 
 impl<'a> From<&'a Automata> for Frame<'a> {
     fn from(automata: &'a Automata) -> Self {
         Self {
             automata,
-            phase: Phase::BeforeStart,
+            progress: automata.into(),
         }
     }
-}
-
-enum Phase {
-    BeforeStart,
-    Processing(Progress),
-    AfterEnd,
 }
 
 enum Progress {
@@ -38,4 +32,21 @@ enum Progress {
     Selection,
     Sequence { processing_element_index: usize },
     StartOfLine,
+}
+
+impl From<&Automata> for Progress {
+    fn from(automata: &Automata) -> Self {
+        match automata {
+            Automata::Character(_) => Self::Character,
+            Automata::EndOfLine => Self::EndOfLine,
+            Automata::Repetition { body: _, number: _ } => Self::Repetition {
+                repetition_count: 0,
+            },
+            Automata::Selection(_) => Self::Selection,
+            Automata::Sequence(_) => Self::Sequence {
+                processing_element_index: 0,
+            },
+            Automata::StartOfLine => Self::StartOfLine,
+        }
+    }
 }
