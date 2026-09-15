a3e6b3dabf516033a32a8ff5ca0e2513ec065373
TaiseiIto <taisei1212@outlook.jp>
2026-09-15 22:21:55 +0900

    This is not a bug

--- tool/regex/src/main.rs
+++ tool/regex/src/main.rs
@@ -1,10 +1,6 @@
 use regex::*;
 
 fn main() {
-    let automata: Automata = dbg!(
-        r"^\d(\l+|\u*)[^a-d0-3_{}]{2,3}[^\w()*]{3}$"
-            .parse()
-            .unwrap()
-    );
+    let automata: Automata = dbg!(r"^\d(\l+|\u*)[a-d0-3_{}]{2,3}[^\w()*]{3}$".parse().unwrap());
     dbg!(automata.input("3a3!!!"));
 }
