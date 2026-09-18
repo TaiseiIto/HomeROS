26a0e1f4211dcefec84855d2d4c8174a159e74a0
TaiseiIto <taisei1212@outlook.jp>
2026-09-18 22:36:13 +0900

    Fix regex test #7

--- tool/regex/src/lib.rs
+++ tool/regex/src/lib.rs
@@ -21,10 +21,20 @@ mod test {
 
     #[test]
     fn test() {
-        let automaton: Automaton = r"^clk(\d+)([kmgtpxzyrq])hz$".parse().unwrap();
-        let matches: Vec<Match> = automaton.input("clk24mhz");
+        let automaton: Automaton = r"^(clk(\d+)([kmgtpxzyrq]?)hz,?){2}$".parse().unwrap();
+        let matches: Vec<Match> = automaton.input("clk24mhz,clk115200hz");
         let captures: Vec<Capture> = matches.iter().flat_map(|mat| mat.captures()).collect();
         let captures: Vec<&str> = captures.iter().map(Into::into).collect();
-        assert_eq!(captures, vec!["clk24mhz", "24", "m"]);
+        assert_eq!(
+            captures,
+            vec![
+                "clk24mhz,clk115200hz",
+                "clk24mhz,",
+                "24",
+                "m",
+                "clk115200hz",
+                "115200"
+            ]
+        );
     }
 }
